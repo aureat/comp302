e4c3b2ca2a05f5dc713ea06be073e09d5c62426b
altunh <hasanlialtun@gmail.com>
2023-05-04 01:08:18 +0300

    patched ui.assets.Fonts

--- conkueror/src/ui/assets/Fonts.java
+++ conkueror/src/ui/assets/Fonts.java
@@ -1,10 +1,12 @@
 package ui.assets;
 
+import javax.swing.*;
 import java.awt.*;
 import java.io.InputStream;
 
 public class Fonts {
 
+    public static Font defaultFont = new JLabel().getFont();
     public static Font GilroyBold;
     public static Font GilroyExtraBold;
 
@@ -16,6 +18,9 @@ public class Fonts {
     public static Font createFont(String name) {
         try {
             InputStream is = Fonts.class.getResourceAsStream("/fonts/" + name + ".ttf");
+            if (is == null) {
+                return defaultFont;
+            }
             return Font.createFont(Font.TRUETYPE_FONT, is);
         } catch (Exception e) {
             e.printStackTrace();
