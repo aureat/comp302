125d38ff25fce6ef06ebd9c132167a4023a73718
Cousim <kguler20@ku.edu.tr>
2023-05-24 04:53:44 +0300

    HelpView fixed

--- conkueror/src/ui/app/views/HelpView.java
+++ conkueror/src/ui/app/views/HelpView.java
@@ -65,19 +65,13 @@ public class HelpView extends ViewPanel<HelpController> {
         boardLabel.setBounds((getContainerWidth()-896)/2,(getContainerHeight()-469)/2,896,469);
 
         JLabel textLabel = new JLabel();
-        textLabel.setBounds(0,0,850,450);
+        textLabel.setBounds(20,0,850,450);
         textLabel.setLayout(new BorderLayout());
+        textLabel.setText("<html>ConKUeror is a multiplayer strategy game that can be played on Windows, MacOS, and Linux.\n The game is about conquering and invading countries in a virtual multiplayer world, and it offers a unique and immersive gaming experience that challenges players' strategic thinking and decision-making skills.\n The game has three types of cards: territory cards, army cards, and chance cards.\n Players can trade army cards to gain an additional army if they have a set of three cards. Each player is given a number of infantry pieces at the beginning of the game, and they take turns placing their armies onto unoccupied territories until all territories are claimed. The game continues until a player has reached complete world domination. A round of the game consists of three phases: draft, attack, and fortify.\n The game can be customized with various features and gameplay options, making it appealing to different interest groups.<br/>");
+        textLabel.setFont(Fonts.GilroyBold.deriveFont(24f));
+        textLabel.setForeground(Color.white);
 
-        JTextField playerName = new JTextField();
-        playerName.setText("ConKUeror is a multiplayer strategy game that can be played on Windows, MacOS, and Linux.\n The game is about conquering and invading countries in a virtual multiplayer world, and it offers a unique and immersive gaming experience that challenges players' strategic thinking and decision-making skills.\n The game has three types of cards: territory cards, army cards, and chance cards.\n Players can trade army cards to gain an additional army if they have a set of three cards. Each player is given a number of infantry pieces at the beginning of the game, and they take turns placing their armies onto unoccupied territories until all territories are claimed. The game continues until a player has reached complete world domination. A round of the game consists of three phases: draft, attack, and fortify.\n The game can be customized with various features and gameplay options, making it appealing to different interest groups.");
-        playerName.setEditable(false);
-        playerName.setBorder(new LineBorder(new Color(1f,0f,0f,0f )));
-        playerName.setFont(Fonts.GilroyBold.deriveFont(24f));
-        playerName.setForeground(Color.white);
-        playerName.setPreferredSize(new Dimension(555,57));
-        playerName.setOpaque(false);
 
-        textLabel.add(playerName);
         boardLabel.add(textLabel);
 
 
